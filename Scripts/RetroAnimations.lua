20084491052ae87c91d5ca5b491bf53f8be1c097
LazyNoob <105753113+LazyNoobGithub@users.noreply.github.com>
2025-05-25 15:22:37 -0400

    Update RetroAnimations.lua

--- Scripts/RetroAnimations.lua
+++ Scripts/RetroAnimations.lua
@@ -1,6 +1,6 @@
 -- animation changer
 
-local db = loadstring(game:HttpGet("https://raw.githubusercontent.com/LazyNoobGithub/UFE/refs/heads/main/Scripts/AnimationDatabase.lua"))()
+local db = loadstring(game:HttpGet("https://raw.githubusercontent.com/LazyNoobGithub/UFE/refs/heads/main/Scripts/AnimationData.lua"))()
 
 function cg(name, value) if _G[name] == nil then _G[name] = value end end
 
